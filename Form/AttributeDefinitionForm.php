72542d8401ad34f2bd959200bf9fa3e8cc18b41c
Amant Arnaud <arnaud.amant@gmail.com>
2017-03-22 08:43:11 +0100

    disabled modification name in form attribute definition

--- Form/AttributeDefinitionForm.php
+++ Form/AttributeDefinitionForm.php
@@ -2,9 +2,12 @@
 
 namespace Aamant\ConfigurableAttributesBundle\Form;
 
+use Aamant\ConfigurableAttributesBundle\Entity\AttributeDefinition;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\Form\FormEvent;
+use Symfony\Component\Form\FormEvents;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class AttributeDefinitionForm extends AbstractType
@@ -15,16 +18,24 @@ class AttributeDefinitionForm extends AbstractType
      */
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
-        $builder
-            ->add('name')
-            ->add('label')
-            ->add('options', CollectionType::class, [
-                'entry_type'    => AttributeDefinitationOptionType::class,
-                'allow_add'     => true,
-                'allow_delete'  => true,
-                'by_reference'  => false,
-            ])
-        ;
+        $builder->addEventListener(FormEvents::PRE_SET_DATA, function (FormEvent $event) {
+            /** @var AttributeDefinition $entity */
+            $entity = $event->getData();
+            $builder = $event->getForm();
+
+            $builder
+                ->add('name', null, [
+                    'disabled' => ($entity->getId() != null)
+                ])
+                ->add('label')
+                ->add('options', CollectionType::class, [
+                    'entry_type'    => AttributeDefinitationOptionType::class,
+                    'allow_add'     => true,
+                    'allow_delete'  => true,
+                    'by_reference'  => false,
+                ])
+            ;
+        });
     }
     
     /**
